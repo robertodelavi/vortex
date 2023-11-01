a17ac417d474c7e4f39d901679e92acb8a694698
Roberto Delavi <roberto.delavy@gmail.com>
2023-11-01 06:45:17 -0300

    FilePond

--- library/vristo/header-main.php
+++ library/vristo/header-main.php
@@ -12,8 +12,8 @@
     <title>Vórtex - Simplificando a sua gestão Imobiliária</title>
 
     <!-- Filepond stylesheet -->
-    <!-- <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet"> -->
-    <link href="<?php echo BASE_THEME_URL; ?>/filepond/dist/filepond.css" rel="stylesheet">
+    <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+    <!-- <link href="<?php echo BASE_THEME_URL; ?>/filepond/dist/filepond.css" rel="stylesheet"> -->
 
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <link rel="icon" type="image/x-icon" href="<?php echo BASE_THEME_URL; ?>/favicon.ico" />
@@ -40,8 +40,8 @@
     <input type="file" class="filepond">
 
     <!-- Load FilePond library -->
-    <!-- <script src="https://unpkg.com/filepond/dist/filepond.js"></script> -->
-    <script src="<?php echo BASE_THEME_URL; ?>/filepond/dist/filepond.js"></script>
+    <script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+    <!-- <script src="<?php echo BASE_THEME_URL; ?>/filepond/dist/filepond.js"></script> -->
 
     <!-- Turn all file input elements into ponds -->
     <script>
